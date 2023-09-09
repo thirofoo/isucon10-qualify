6ef5d65b9d0f0fac980bf7179d59ed319fb9295c
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-09 16:06:25 +0900

    index変更

--- mysql/db/3_CreateIndex.sql
+++ mysql/db/3_CreateIndex.sql
@@ -1,3 +1,3 @@
 USE isuumo;
-CREATE INDEX chair_popularity_idx ON isuumo.chair (popularity);
-CREATE INDEX estate_popularity_idx ON isuumo.estate (popularity);
+CREATE INDEX chair_popularity_id_index ON chair (popularity DESC, id ASC);
+CREATE INDEX estate_popularity_id_index ON estate (popularity DESC, id ASC);
