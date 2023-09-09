d0521444527e33e520a29475d462f1c73282caaa
Daaaai0809 <dai.tsuruga@80and.co>
2023-09-09 16:57:09 +0900

    nazotte 修正

--- go/main.go
+++ go/main.go
@@ -852,57 +852,54 @@ func searchRecommendedEstateWithChair(c echo.Context) error {
 
 func searchEstateNazotte(c echo.Context) error {
 	coordinates := Coordinates{}
-	err := c.Bind(&coordinates)
-	if err != nil {
-		c.Echo().Logger.Infof("post search estate nazotte failed : %v", err)
-		return c.NoContent(http.StatusBadRequest)
-	}
+    err := c.Bind(&coordinates)
+    if err != nil {
+        c.Echo().Logger.Infof("post search estate nazotte failed : %v", err)
+        return c.NoContent(http.StatusBadRequest)
+    }
 
-	if len(coordinates.Coordinates) == 0 {
-		return c.NoContent(http.StatusBadRequest)
-	}
+    if len(coordinates.Coordinates) == 0 {
+        return c.NoContent(http.StatusBadRequest)
+    }
 
-	b := coordinates.getBoundingBox()
-	estatesInBoundingBox := []Estate{}
-	query := `SELECT * FROM estate WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? ORDER BY popularity DESC, id ASC`
-	err = db.Select(&estatesInBoundingBox, query, b.BottomRightCorner.Latitude, b.TopLeftCorner.Latitude, b.BottomRightCorner.Longitude, b.TopLeftCorner.Longitude)
-	if err == sql.ErrNoRows {
-		c.Echo().Logger.Infof("select * from estate where latitude ...", err)
-		return c.JSON(http.StatusOK, EstateSearchResponse{Count: 0, Estates: []Estate{}})
-	} else if err != nil {
-		c.Echo().Logger.Errorf("database execution error : %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
+    // 1. 範囲内のエステートを取得するSQLクエリを構築
+    bboxQuery := `
+        SELECT * FROM estate 
+        WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ? 
+        ORDER BY popularity DESC, id ASC
+    `
+    bboxParams := []interface{}{coordinates.BottomRightCorner.Latitude, coordinates.TopLeftCorner.Latitude, coordinates.BottomRightCorner.Longitude, coordinates.TopLeftCorner.Longitude}
 
-	estatesInPolygon := []Estate{}
-	for _, estate := range estatesInBoundingBox {
-		validatedEstate := Estate{}
+    // 2. 地理的な条件を一括で検証するためのPolygonを取得
+    polygonText := coordinates.coordinatesToText()
 
-		point := fmt.Sprintf("'POINT(%f %f)'", estate.Latitude, estate.Longitude)
-		query := fmt.Sprintf(`SELECT * FROM estate WHERE id = ? AND ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText(%s))`, coordinates.coordinatesToText(), point)
-		err = db.Get(&validatedEstate, query, estate.ID)
-		if err != nil {
-			if err == sql.ErrNoRows {
-				continue
-			} else {
-				c.Echo().Logger.Errorf("db access is failed on executing validate if estate is in polygon : %v", err)
-				return c.NoContent(http.StatusInternalServerError)
-			}
-		} else {
-			estatesInPolygon = append(estatesInPolygon, validatedEstate)
-		}
-	}
+    // 3. 一括でエステートを検証するクエリを構築
+    validateQuery := fmt.Sprintf(`
+        SELECT estate.* FROM estate 
+        JOIN (
+            SELECT id FROM estate
+            WHERE latitude <= ? AND latitude >= ? AND longitude <= ? AND longitude >= ?
+        ) AS bbox_estates
+        ON estate.id = bbox_estates.id
+        WHERE ST_Contains(ST_PolygonFromText(%s), ST_GeomFromText('POINT(' || estate.latitude || ' ' || estate.longitude || ')'))
+    `, polygonText)
 
-	var re EstateSearchResponse
-	re.Estates = []Estate{}
-	if len(estatesInPolygon) > NazotteLimit {
-		re.Estates = estatesInPolygon[:NazotteLimit]
-	} else {
-		re.Estates = estatesInPolygon
-	}
-	re.Count = int64(len(re.Estates))
+    validateParams := []interface{}{coordinates.BottomRightCorner.Latitude, coordinates.TopLeftCorner.Latitude, coordinates.BottomRightCorner.Longitude, coordinates.TopLeftCorner.Longitude}
 
-	return c.JSON(http.StatusOK, re)
+    // 4. 一括でエステートを取得
+    var estatesInPolygon []Estate
+    err := db.Select(&estatesInPolygon, validateQuery, validateParams...)
+    if err != nil {
+        c.Echo().Logger.Errorf("db access failed on executing validate if estate is in polygon: %v", err)
+        return c.NoContent(http.StatusInternalServerError)
+    }
+
+    // 5. レスポンスを構築
+    var re EstateSearchResponse
+    re.Estates = estatesInPolygon
+    re.Count = int64(len(estatesInPolygon))
+
+    return c.JSON(http.StatusOK, re)
 }
 
 func postEstateRequestDocument(c echo.Context) error {
