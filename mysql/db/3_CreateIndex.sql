77077777732a3182512f98c88eab20d2fff831a1
thirofoo <83126064+thirofoo@users.noreply.github.com>
2023-09-09 16:39:56 +0900

    popularity index 復活!

--- mysql/db/3_CreateIndex.sql
+++ mysql/db/3_CreateIndex.sql
@@ -1,3 +1,5 @@
 USE isuumo;
+CREATE INDEX chair_popularity_id_index ON chair (popularity DESC, id ASC);
+CREATE INDEX estate_popularity_id_index ON estate (popularity DESC, id ASC);
 CREATE INDEX chair_kind_idx ON isuumo.chair (`kind`);
 CREATE INDEX chair_color_idx ON isuumo.chair (`color`);
